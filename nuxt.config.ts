6c30abbfcd903e07aff5f28fb9924c2aab9bf508
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-08-19 12:41:40 +0200

    feat(sentry): add Sentry client and server configuration files
    - Created `sentry.client.config.ts` for client-side Sentry integration with session replay and logging enabled.
    - Created `sentry.server.config.ts` for server-side Sentry integration with logging enabled.
    - Added example API route `sentry-example-api.ts` that throws an error to demonstrate Sentry error tracking.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,42 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  routeRules: {
+    '/**': { swr: 3600 },
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@sentry/nuxt/module'
   ],
+
   css: ['assets/css/main.css'],
 
   runtimeConfig: {
+    public: {
+      sentry: {
+        dsn: ''
+      }
+    },
     ticketmaster: {
       apiKey: '',
     }
+  },
+
+  sentry: {
+    sourceMapsUploadOptions: {
+      org: 'crewbe',
+      project: 'tickets'
+    },
+
+    autoInjectServerSentry: 'top-level-import'
+  },
+
+  sourcemap: {
+    client: 'hidden'
   }
 })
