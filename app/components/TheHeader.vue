4fb1cda6cd394851f7911e429b02f034747960fa
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-08-26 11:40:50 +0200

    fix: add classes for improved styling and active link indication in TheHeader component

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -2,10 +2,14 @@
   <div class="flex justify-between items-center p-4 bg-gray-800 text-white">
     <div>TICKETS</div>
     <div class="space-x-4">
-      <NuxtLink to="/">Eventi</NuxtLink>
-      <NuxtLink to="/attractions">Artisti</NuxtLink>
-      <NuxtLink to="/classifications">Generi</NuxtLink>
-      <NuxtLink to="/venues">Luoghi</NuxtLink>
+      <NuxtLink to="/" class="pb-0.75 border-b-2 border-transparent hover:border-white transition"
+        :class="{ 'border-white': $route.path === '/' }">Eventi</NuxtLink>
+      <NuxtLink to="/attractions" class="pb-0.75 border-b-2 border-transparent hover:border-white transition"
+        :class="{ 'border-white': $route.path === '/attractions' }">Artisti</NuxtLink>
+      <NuxtLink to="/classifications" class="pb-0.75 border-b-2 border-transparent hover:border-white transition"
+        :class="{ 'border-white': $route.path === '/classifications' }">Generi</NuxtLink>
+      <NuxtLink to="/venues" class="pb-0.75 border-b-2 border-transparent hover:border-white transition"
+        :class="{ 'border-white': $route.path === '/venues' }">Luoghi</NuxtLink>
     </div>
   </div>
 </template>
